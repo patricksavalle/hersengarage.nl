52adcf724771e9d4eb2e7ba61271a0356f10f968
Patrick Savalle <patrick@patricksavalle.com>
2020-08-30 21:43:47 +0200

    First version of addon. Works fine.

---
+++ system/user/addons/embedder/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author'      => 'Patrick',
+	'author_url'  => 'http://patricksavalle.com/',
+	'name'        => 'Embedder',
+	'description' => 'Transform inline links into embeds',
+	'version'     => '0.1',
+	'namespace'   => 'User\Addons\Embedder',
+	'settings_exist' => FALSE
+);
---
+++ system/user/addons/embedder/pi.embedder.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Embedder {
+
+	var $return_data;
+
+
+	function __construct()
+	{
+		$str = ee()->TMPL->tagdata;
+
+		// transform youtube-link into embed
+		$str = preg_replace(
+			"/\[\[https:\/\/(?:(?:www|m)\.)?(?:youtube\.com|youtu.be)\/(?:[\w\-]+\?v=|embed\/|v\/?)?([\w\-]+)(?:\S+)?\]\]/",
+			"<iframe class='eb_video eb_youtube' src='//www.youtube.com/embed/$1' allowfullscreen></iframe>",
+			$str);
+
+		// transform bitchute-link into embed
+		$str = preg_replace(
+			"/\[\[https?:\/\/(?:www)?\.?bitchute\.com\/video\/([\w-]{1,64})\/\]\]/",
+			"<iframe class='eb_video eb_bitchute' src='//www.bitchute.com/embed/$1' allowfullscreen></iframe>",
+			$str);
+
+		// else assume image
+		$str = preg_replace(
+			"/\[\[(https?:\/\/.+)\]\]/",
+			"<img src='$1'>",
+			$str);
+
+		$this->return_data = $str;
+	}
+}
+// END CLASS
